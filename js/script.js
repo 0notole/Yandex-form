c2b475b96523b82a0cbd8e24e17a237386efbc6a
Anatoly <silko.a@gmail.com>
2013-08-09 02:03:04 +0400

    Almost

--- js/script.js
+++ js/script.js
@@ -32,4 +32,54 @@ $(document).ready(function(){
 			}
 	});
 	
+	$("input").on("focusout change", function(){
+		if(($(this).attr("required") && $(this).val().trim() == '') || ($(this).attr("type") == 'checkbox' && !$(this).is(':checked')))
+			{
+				if($(this).attr("type") == 'checkbox')
+					{
+						$(this).parent().next("div.hint").addClass("required").html($(this).data("message"));
+					}
+				else
+					{
+						$(this).next("div.hint").addClass("required").html($(this).data("message"));	
+					}
+			}
+		else
+			{
+				
+				if($(this).attr("type") == 'checkbox')
+					{
+						$(this).parent().next("div.hint.required").empty();
+					}
+				else
+					{
+						$(this).next("div.hint.required").empty();
+					}
+			}
+	});
+	
+	$("select").on("focusout change", function(){
+		if($(this).val() == "select")
+			{
+				$(this).next("div.hint").addClass("required").html($(this).data("message"));
+			}
+		else
+			{
+				$(this).next("div.hint.required").empty();
+			}
+	});
+	
+	$("input#file").on("change", function(){
+		var name = this.value;
+		reWin = /.*\\(.*)/;
+		var fileTitle = name.replace(reWin, "$1");
+		reUnix = /.*\/(.*)/;
+		fileTitle = fileTitle.replace(reUnix, "$1");
+		$('span#fileName').html(fileTitle);
+	});
+	
 });
+
+
+
+
